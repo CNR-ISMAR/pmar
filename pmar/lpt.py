4431dedb37d41fd018701053aa3cb5f51f9e88de
sofbo <squalobalena5555@gmail.com>
2022-10-19 15:43:33 +0000

    improved init method, added z_bounds, option to use * in filename

--- pmar/lpt.py
+++ pmar/lpt.py
@@ -65,39 +65,51 @@ class LagrangianDispersion(object):
     """
     
     
-    def __init__(self, basedir='lpt_output', uv_path='cmems', wind_path='cmems', mld_path='cmems'):
+    def __init__(self, basin, basedir='lpt_output', uv_path='cmems', wind_path='cmems', mld_path='cmems', particle_path=None, depth=False):
         """
         Parameters
         ----------
-        depth : bool
-            boolean stating whether particle simulation is 2D (depth==False) or 3D (depth==True)
-        o : OceanDrift
-            OpenDrift object producing particle simulation
-        poly_path : str
-            path to shapefile containing polygon used for particle seeding
-        bathy_path : str
-            path to netcdf file for bathymetry (GEBCO)
-        particle_path : str
-            path to netcdf file containing output of OpenDrift simulation
-        raster : xarray
-            xarray object containing output of particle_raster() method
+        basin : str
+            Either one of 'med' for Mediterranean Sea or 'bs' for Black Sea. 
+        basedir : str, optional
+            path to the base directory where all output will be stored. Default is to create a directory called 'lpt output' in the current directory.
+        uv_path : str, optional
+            path to the netcdf file containing ocean current data. Default is 'cmems', meaning CMEMS data will be streamed from Copernicus.
+        wind_path : str, optional
+            path to the netcdf file containing wind velocity data. Default is 'cmems', meaning CMEMS data will be streamed from Copernicus.        
+        mld_path : str, optional
+            path to the netcdf file containing mixed layer depth data. Default is 'cmems', meaning CMEMS data will be streamed from Copernicus.  
+        particle_path : str, optional
+            path to netcdf file containing output of OpenDrift simulation. Default is None. If a particle_path is given in initialisation,    
+        depth : bool, optional
+            boolean stating whether particle simulation is 2D (depth==False) or 3D (depth==True). Default is False
         """
+        # raise error if unsupported basin is requested
+        if basin != 'med' and basin != 'bs':
+            raise ValueError("Method is currently optimised for Med Sea and Black Sea only, thus 'basin' must be one of 'med' or 'bs'.")
+        
+        self.basin = basin
         Path(basedir).mkdir(parents=True, exist_ok=True)
         self.uv_path = uv_path
         self.wind_path = wind_path
         self.mld_path = mld_path # so that one can also specify a local path to data file. default is 'cmems' meaning we stream the data from copernicus
         self.basedir = Path(basedir)
-        self.depth = None
+        self.particle_path = particle_path
+        
+        if self.particle_path is not None: #take depth value from filename
+            self.depth = bool(self.particle_path.split('depth_')[1].split('.')[0])
+        else:
+            self.depth = depth
+            
         self.o = None
-        self.poly_path = None
-        self.bathy_path = None
-        self.particle_path = None
+        self.poly_path = None # could give bath to full baisn as default
+        self.bathy_path = None # need to give path to gebco bathymetry as default 
         self.raster = None
         self.ds = None
         self.origin_marker = None
         pass
     
-    def run(self, pnum, basin, time, repeat_run=1, res=0.04, crs='4326', lon=None, lat=None, z=-0.5, depth=False, tstep=6, hdiff=10, termvel=1e-3, bounds=None, use_path='even_dist', depth_layer='water_column', decay_rate=None, loglevel=20, save_to=None):         
+    def run(self, pnum, time, repeat_run=1, res=0.04, crs='4326', lon=None, lat=None, z=-0.5, tstep=6, hdiff=10, termvel=1e-3, bounds=None, use_path='even_dist', decay_rate=None, depth_layer='water_column', z_bounds=[10,100], loglevel=20, save_to=None):         
         """
         Launches methods particle_simulation and particle_raster. 
         
@@ -106,8 +118,6 @@ class LagrangianDispersion(object):
         ----------
         pnum : int 
             The number of particles to be seeded
-        basin : str
-            Either 'med' for Mediterranean Sea or 'bs' for Black Sea
         time : list 
             List of strings giving the time bounds (start time and end time) for the particle simulation
         repeat_run = int, optional
@@ -122,8 +132,6 @@ class LagrangianDispersion(object):
             List giving two latitudinal bounds for particle seeding. Default is None (full basin)
         z : float, optional
             Depth at which to seed particles in [m]. Default is -0.5m. 
-        depth : bool, optional
-            Boolean stating whether simulation should be 2D (depth == False) or 3D (depth == True). Default is False
         tstep : int, optional
             Time step used for OpenDrift simulation, in hours. Default is 6
         hdiff : float, optional
@@ -136,7 +144,7 @@ class LagrangianDispersion(object):
             Path to .tif file representing density of human use of marine environment, used for 'weights' of particles in histogram calculation. 
             If no use_path is given, a weight of 1 is given to all particles ('even_dist' for even distribution).
         depth_layer : str, optional
-            Depth layer chosen for computing histogram. One of 'water_column', 'surface' or 'seafloor'. Default is 'water_column'
+            Depth layer chosen for computing histogram. One of 'full_depth', 'water_column', 'surface' or 'seafloor'. Default is 'full_depth'
         decay_rate : float, optional
             Decay rate of particles in [days-1]. Default is None
         loglevel : int, optional
@@ -146,6 +154,10 @@ class LagrangianDispersion(object):
             One of 'tif' or 'netcdf'. Writes raster to either one extension
         
         """
+        # raise error if particle_path is already given
+        if self.particle_path is not None:
+            raise ValueError("Cannot use 'run' method if a 'particle_path' is already given. In this case, launch method 'particle_raster' directly.")
+        
         
         # this gives the option of doing e.g. monthly runs to avoid crashing when seeding a lot of particles. 
         t0 = datetime.strptime(time[0], '%Y-%m-%d')
@@ -153,19 +165,24 @@ class LagrangianDispersion(object):
         iter_dt = t1-t0
         particle_list = {}
 
+        
         for i in range(0, repeat_run):
             t0 = t1
             t1 = t0+iter_dt
             
             self.origin_marker = i 
 
-            self.particle_simulation(pnum=pnum, basin=basin, time=[t0.strftime('%Y-%m-%d'), t1.strftime('%Y-%m-%d')], res=res, crs=crs, lon=lon, lat=lat, z=z, depth=depth, tstep=tstep, hdiff=hdiff, termvel=termvel, loglevel=loglevel)    
+            self.particle_simulation(pnum=pnum, time=[t0.strftime('%Y-%m-%d'), t1.strftime('%Y-%m-%d')], res=res, crs=crs, lon=lon, lat=lat, z=z, tstep=tstep, hdiff=hdiff, termvel=termvel, loglevel=loglevel)    
             particle_list[i] = self.particle_path
-            
-        self.particle_path = particle_list
-            
+                
+        if len(particle_list) == 1:
+            self.particle_path == list(particle_list.values())[0]
+        else:
+            self.particle_path = particle_list
+        
+        
         # compute raster
-        self.particle_raster(res=res, crs=crs, tstep=tstep, bounds=bounds, use_path=use_path, depth_layer=depth_layer, decay_rate=decay_rate, save_to=save_to)
+        self.particle_raster(res=res, crs=crs, tstep=tstep, bounds=bounds, use_path=use_path, decay_rate=decay_rate, depth_layer=depth_layer, z_bounds=z_bounds, save_to=save_to)
         
         
         
@@ -227,7 +244,7 @@ class LagrangianDispersion(object):
     
 
     
-    def particle_simulation(self, pnum, basin, time, res=0.04, crs='4326', lon=None, lat=None, z=-0.5, depth=False, tstep=6, hdiff=10, termvel=1e-3, loglevel=20):
+    def particle_simulation(self, pnum, time, res=0.04, crs='4326', lon=None, lat=None, z=-0.5, tstep=6, hdiff=10, termvel=1e-3, loglevel=20):
         """
         Method to start a trajectory simulation, after initial configuration, using OpenDrift by MET Norway.
         Uses OceanDrift module. 
@@ -242,8 +259,6 @@ class LagrangianDispersion(object):
         ----------
         pnum : int 
             The number of particles to be seeded
-        basin : str
-            Either 'med' for Mediterranean Sea or 'bs' for Black Sea
         time : list 
             List of strings giving the time bounds (start time and end time) for the particle simulation
         res : float, optional
@@ -256,8 +271,6 @@ class LagrangianDispersion(object):
             List giving two latitudinal bounds for particle seeding. Default is None (full basin)
         z : float, optional
             Depth at which to seed particles in [m]. Default is -0.5m. 
-        depth : bool, optional
-            Boolean stating whether simulation should be 2D (depth == False) or 3D (depth == True). Default is False
         tstep : int, optional
             Time step used for OpenDrift simulation, in hours. Default is 6
         hdiff : float, optional
@@ -271,12 +284,9 @@ class LagrangianDispersion(object):
         
         data_path = Path('/home/sbosi/data/')
         
-        self.depth = depth
-        self.o = OceanDrift(loglevel=loglevel) # initialise OpenDrift object
+        Path(self.basedir / 'particles').mkdir(parents=True, exist_ok=True)
         
-        # raise error if unsupported basin is requested
-        if basin != 'med' and basin != 'bs':
-            raise ValueError("Method is currently optimised for Med Sea and Black Sea only, thus 'basin' must be one of 'med' or 'bs'.")
+        self.o = OceanDrift(loglevel=loglevel) # initialise OpenDrift object
         
         
         # polygon used for seeding of particles. by default, it is the whole basin. but if lon and lat are given, a new polygon is created using those bounds. 
@@ -284,13 +294,13 @@ class LagrangianDispersion(object):
             self.make_poly(lon, lat, crs=crs)
         else:
             if self.poly_path is None:
-                self.poly_path = f'/home/sbosi/data/polygons/polygon-{basin}-full-basin.shp'
+                self.poly_path = f'/home/sbosi/data/polygons/polygon-{str(self.basin)}-full-basin.shp'
         
         bds = np.round(gpd.read_file(self.poly_path).total_bounds) # only used in particle_path
        
         # stream data from Copernicus CMEMS (currents, wind)
         if self.uv_path == 'cmems':
-            DATASET_ID = f'{basin.replace("full_", "")}-cmcc-cur-rean-d' #ocean currents
+            DATASET_ID = f'{str(self.basin).replace("full_", "")}-cmcc-cur-rean-d' #ocean currents
             self.o.add_readers_from_list([f'https://my.cmems-du.eu/thredds/dodsC/{DATASET_ID}'])
         elif self.uv_path != 'cmems': 
             uv_reader = reader_netCDF_CF_generic.Reader(self.uv_path)
@@ -309,7 +319,7 @@ class LagrangianDispersion(object):
             raise ValueError('path to wind data file not recognised')            
         
         # bathymetry, mixed layer depth (3D case)
-        if depth == True:
+        if self.depth == True:
             # bathymetry (gebco)
             bathy_file = 'bathymetry_gebco_2022_n46.8018_s29.1797_w-6.5918_e43.8574.nc'
             self.bathy_path = str(data_path / 'input' / bathy_file)
@@ -318,7 +328,7 @@ class LagrangianDispersion(object):
             
             if self.mld_path == 'cmems':
                 # mixed layer depth (cmems)
-                mld_ID = f'{basin}-cmcc-mld-rean-d'
+                mld_ID = f'{str(self.basin)}-cmcc-mld-rean-d'
                 self.o.add_readers_from_list([f'https://my.cmems-du.eu/thredds/dodsC/{DATASET_ID}'])
             elif self.mld_path != 'cmems':
                 mld_reader = reader_netCDF_CF_generic.Reader(self.mld_path)
@@ -332,8 +342,8 @@ class LagrangianDispersion(object):
         end_time = datetime.strptime(time[1], '%Y-%m-%d') 
         
         # path to write particle simulation file. used for our 'cache'
-        particle_path = f'{basin}-lon_{int(bds[0])}-{int(bds[2])}_lat_{int(bds[1])}-{int(bds[3])}-pnum_{pnum}-time_{time[0]}_to_{time[1]}-tstep_{tstep}hrs-hdiff_{hdiff}-termvel_{termvel}-depth_{depth}.nc'
-        q =  self.basedir / particle_path
+        particle_path = f'{str(self.basin)}-lon_{int(bds[0])}-{int(bds[2])}_lat_{int(bds[1])}-{int(bds[3])}-pnum_{pnum}-time_{time[0]}_to_{time[1]}-tstep_{tstep}hrs-hdiff_{hdiff}-termvel_{termvel}-depth_{str(self.depth)}.nc'
+        q =  self.basedir / 'particles' / particle_path
             
         # if a file with that name already exists, simply import it using OpenDrift import method. 
         if q.exists() == True:
@@ -344,7 +354,7 @@ class LagrangianDispersion(object):
 
             self.o.set_config('drift:horizontal_diffusivity', hdiff)    
             
-            if depth == True:
+            if self.depth == True:
                 # if simulation is 3D, set 3D parameters (terminal velocity, vertical mixing, seafloor action) and seed particles over polygon
                 self.o.set_config('seed:terminal_velocity', termvel)
                 self.o.seed_from_shapefile(str(self.poly_path), number=pnum, time=start_time, terminal_velocity=termvel, z=z, origin_marker=self.origin_marker)
@@ -357,7 +367,7 @@ class LagrangianDispersion(object):
                 self.o.seed_from_shapefile(str(self.poly_path), number=pnum, time=start_time, origin_marker=self.origin_marker)
             
             # run simulation and write to temporary file
-            self.o.run(end_time=end_time, time_step=time_step, outfile='temp_particle_file.nc')
+            self.o.run(end_time=end_time, time_step=time_step, outfile=str(self.basedir)+'/temp_particle_file.nc')
 
         # open temporary file to handle 'inactive' particles (i.e. particles that have beached or gotten stuck on seafloor). 
         # inactive particles are kept visible rather than deactivated, so they contribute to final count in particle_raster method. 
@@ -369,12 +379,12 @@ class LagrangianDispersion(object):
         
         self.ds = ps
         
-        self.particle_path = particle_path
+        self.particle_path = str(q) #particle_path
 
         pass     
     
 
-    def particle_raster(self, res=0.04, crs='4326', tstep=None, bounds=None, use_path='even_dist', depth_layer='water_column', decay_rate=None, save_to=None):
+    def particle_raster(self, res=0.04, crs='4326', tstep=None, bounds=None, use_path='even_dist', decay_rate=None, depth_layer='full_depth', z_bounds=[10,100], save_to=None):
         """
         Method to compute a 2D horizontal histogram of particle concentration using the xhistogram.xarray package. 
         
@@ -395,10 +405,12 @@ class LagrangianDispersion(object):
         use_path : str, optional
             Path to file representing density of human use, used for 'weights' of particles in histogram calculation. 
             If no use_path is given, a weight of 1 is given to all particles. Default is None
-        depth_layer : str, optional
-            Depth layer chosen for computing histogram. One of 'water_column', 'surface' or 'seafloor'. Default is 'water_column'
         decay_rate : float, optional
             Decay rate of particles in [days-1]. Default is None
+        depth_layer : str, optional
+            Depth layer chosen for computing histogram. One of 'full_depth', 'water_column', 'surface' or 'seafloor'. Default is 'full_depth'
+        z_bounds : list, optional
+            Two parameters, given as z_bounds=[z_surface, z_bottom], determining the depth layers' thickness in [m]. The first represents vertical distance from the ocean surface (z=0), whhile the second represents vertical distance from the ocean bottom, given by the bathymetry. Default is z_bounds=[10,100].
         save_to : str, optional
             One of 'tif' or 'netcdf'. Writes raster to either one extension
         """
@@ -415,20 +427,20 @@ class LagrangianDispersion(object):
             else:
                 particle_path = self.particle_path
 
-            basin = particle_path.split('-',1)[0]
-            self.poly_path = self.poly_path = f'/home/sbosi/data/polygons/polygon-{basin}-full-basin.shp'
+            #basin = particle_path.split('-',1)[0]
+            self.poly_path = self.poly_path = f'/home/sbosi/data/polygons/polygon-{str(self.basin)}-full-basin.shp'
         else:
             pass
-        
-        if len(self.particle_path) > 1:
+
+        if type(self.particle_path) == dict: 
             for i in self.particle_path:
                 path_list = self.particle_path
                 path_list[i] = str(self.basedir / path_list[i])
             
             _ds = xr.open_mfdataset(path_list.values(), concat_dim='trajectory', combine='nested', join='override').isel(time=slice(1,None)).chunk({'trajectory': 10000, 'time':1000}) # removing time 0 
         else:
-            q = self.basedir / list(self.particle_path.values())[0]
-            _ds = xr.open_mfdataset(str(q), concat_dim='trajectory', combine='nested', join='override').isel(time=slice(1,None)).chunk({'trajectory': 10000, 'time':1000}) # removing time 0
+            #q = self.basedir / list(self.particle_path)[0]
+            _ds = xr.open_mfdataset(str(self.particle_path), concat_dim='trajectory', combine='nested', join='override').isel(time=slice(1,None)).chunk({'trajectory': 10000, 'time':1000}) # removing time 0
 
         
         _ds['trajectory'] = np.arange(0, len(_ds.trajectory)) # give trajectories unique ID
@@ -506,22 +518,25 @@ class LagrangianDispersion(object):
         # 3D
         elif self.depth == True:
             ds = ds.assign(depth=-ds.z)
-            #zbin = np.insert(uv.depth, 0, 0).values
             
             # in 3D case, in addition to merge alg we also have options for vertical layers
             if depth_layer == 'surface':
-                surface = ds.where(ds.depth<=10)
+                surface = ds.where(ds.depth<=z_bounds[0])
                 _h = histogram(surface.lon, surface.lat, bins=[xbin, ybin], dim=['trajectory', 'time'], weights=surface.weight)
                 
             elif depth_layer == 'seafloor':
-                seafloor = ds.where(ds.depth>(ds.sea_floor_depth_below_sea_level-100))
+                seafloor = ds.where(ds.depth>(ds.sea_floor_depth_below_sea_level-z_bounds[1]))
                 _h = histogram(seafloor.lon, seafloor.lat, bins=[xbin, ybin], dim=['trajectory', 'time'], weights=seafloor.weight)
             
             elif depth_layer == 'water_column':
+                column = ds.where(np.logical_and(ds.depth>z_bounds[0], ds.depth<z_bounds[1]))
+                _h = histogram(column.lon, column.lat, bins=[xbin, ybin], dim=['trajectory', 'time'], weights=column.weight)            
+            
+            elif depth_layer == 'full_depth':
                 _h = histogram(ds.lon, ds.lat, bins=[xbin, ybin], dim=['trajectory', 'time'], weights=ds.weight)
             
             else:
-                raise ValueError('"depth_layer" must be one of "surface", "seafloor" or "water_column"')    
+                raise ValueError('"depth_layer" must be one of "full_depth", "surface", "seafloor" or "water_column"')    
         
         else:
             raise ValueError('cannot detect whether 2D or 3D')
