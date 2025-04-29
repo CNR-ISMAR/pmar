4a56e2096a30c60eae681a6cf29838db2762564e
sofbo <squalobalena5555@gmail.com>
2025-04-29 08:24:26 +0000

    added get_marine_polygon

--- pmar/pmar_utils.py
+++ pmar/pmar_utils.py
@@ -1,14 +1,28 @@
 import numpy as np
 import xarray as xr
 from copy import deepcopy
+import cartopy.io.shapereader as shpreader
 
-def traj_distinct(bin_n, weight):
-    print(f"bin_n: {bin_n.shape} | weight: {weight.shape}")
-    w = deepcopy(weight)
-    for t in range(1,len(bin_n)): # i leave the first element as 1, otherwise the loop would compare it to the last element.
-        if bin_n[t] == bin_n[t-1]:
-            w[t] = 0
-    return w
+
+def get_marine_polygon(basin=None):
+    '''
+    Returns gdf with polygon of requested basin, to be used with OpenDrift's seed_from_polygon.
+    Default (None) returns global ocean.
+    '''
+    # landmask from cartopy 
+    shpfilename = shpreader.natural_earth(resolution='10m',
+                            category='physical',
+                            name='geography_marine_polys')
+    
+    gdf = gpd.read_file(shpfilename)
+
+    if basin is None:
+        marine_poly = gdf[['geometry']]
+    else:
+        marine_poly = gdf.set_index('label').loc[[str(basin)]][['geometry']]
+
+    return marine_poly
+    
 
 def rasterhd2raster(raster_hd, grid):   
     '''
@@ -39,3 +53,12 @@ def check_particle_file(path_to_file):
             print(f'ATTENTION: all 0 values detected for variable {i}.')
         else:
             print(f'all good: variable {i} has non-zero values.')
+            
+
+def traj_distinct(bin_n, weight):
+    print(f"bin_n: {bin_n.shape} | weight: {weight.shape}")
+    w = deepcopy(weight)
+    for t in range(1,len(bin_n)): # i leave the first element as 1, otherwise the loop would compare it to the last element.
+        if bin_n[t] == bin_n[t-1]:
+            w[t] = 0
+    return w
